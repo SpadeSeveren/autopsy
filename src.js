841dc07db136cb7ceb6b571a871e15cb721d1616
root <root@localhost.localdomain>
2020-02-01 22:07:13 -0700

    src/main splitout

---
+++ src.js
@@ -0,0 +1,193 @@
+let fs = require('fs');
+const slash = process.platform === 'darwin' || process.platform === 'linux' ? '/' : '\\';
+let {points, available} = require(`..${slash}data.json`);
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+function musingButtonCallback(bodyPart, musing) {
+
+    var container = document.getElementsByClassName("musing_table")[0];
+    container.parentNode.removeChild(container);
+
+    var textElement = document.createElement("button");
+    textElement.innerHTML = require(`..${slash}lib${slash}text.js`).fetchMusingText(this.bodyPart, this.innerHTML)[0];
+    textElement.musing = this.innerHTML;
+    textElement.count = 0;
+    textElement.bodyPart = this.bodyPart;
+    textElement.dialogueLength = require(`..${slash}lib${slash}text.js`).fetchMusingText(this.bodyPart, this.innerHTML).length;
+    textElement.className = "dialogue_text";
+    textElement.addEventListener("click", advanceDialogue);
+
+    var newContainer = document.createElement("container");
+    newContainer.className = "dialogue_container";
+
+    var body = document.body;
+    newContainer.appendChild(textElement);
+
+    document.body.appendChild(newContainer);
+
+}
+
+function dialogueButtonCallback(bodyPart, musing) {
+
+    var container = document.getElementsByClassName("dialogue_container")[0];
+    container.parentNode.removeChild(container);
+
+    console.log(bodyPart);
+    console.log(musing);
+    var textElement = document.createElement("button");
+    textElement.innerHTML = require(`..${slash}lib${slash}text.js`).fetchArthurResponse(bodyPart, musing)[0];
+    textElement.musing = musing;
+    textElement.count = 0;
+    textElement.bodyPart = bodyPart;
+    textElement.dialogueLength = require(`..${slash}lib${slash}text.js`).fetchArthurResponse(bodyPart, musing).length;
+    textElement.className = "dialogue_text";
+    textElement.addEventListener("click", advanceArthurDialogue);
+
+    var newContainer = document.createElement("container");
+    newContainer.className = "dialogue_container";
+
+    var body = document.body;
+    newContainer.appendChild(textElement);
+
+    document.body.appendChild(newContainer);
+
+}
+
+function advanceDialogue() {
+    if (this.count == this.dialogueLength - 1) {
+        promptDialogue(this);
+    }
+    else {
+        this.count++;
+        this.innerHTML = require(`..${slash}lib${slash}text.js`).fetchMusingText(this.bodyPart, this.musing)[this.count];
+    }
+}
+
+function advanceArthurDialogue() {
+    if (this.count == this.dialogueLength - 1) {
+        points = require(`..${slash}lib${slash}text.js`).increasePoints(points, this.bodyPart, this.musing);
+        available[this.bodyPart] = false;
+        console.log('available')
+        console.log(available)
+        this.innerHTML = "Return to the full-body examination";
+        this.onclick = () => {
+            loadMusings("body");
+        };
+    }
+    else {
+        this.count++;
+        this.innerHTML = require(`..${slash}lib${slash}text.js`).fetchArthurResponse(this.bodyPart, this.musing)[this.count];
+    }
+}
+
+function loadMusings(bodyPart) {
+    fs.writeFileSync(`./data.json`, JSON.stringify({points: points, available: available}), 'utf8');
+    window.location.href = `./${bodyPart}.html`;
+}
+
+function promptDialogue(element) {
+
+    element.parentNode.removeChild(element);
+
+    var container = document.getElementsByClassName("dialogue_container")[0];
+    var prompt = document.createElement("div");
+    prompt.innerHTML = "Would you like to tell your thoughts to Arthur?";
+    container.appendChild(prompt);
+
+    var table = document.createElement("table");
+    table.className = "yes_no_table";
+    var row = document.createElement("th");
+
+    var yesButton = document.createElement("button");
+    var noButton = document.createElement("button");
+    yesButton.innerHTML = "Yes";
+    noButton.innerHTML = "No";
+    noButton.onclick = () => {
+        loadMusings(element.bodyPart);
+    };
+
+    yesButton.onclick = () => {
+        dialogueButtonCallback(element.bodyPart, element.musing);
+    };
+    row.appendChild(yesButton);
+    row.appendChild(noButton);
+    table.appendChild(row);
+    container.appendChild(table);
+}
+
+function createMusings(bodyPart) {
+    var musingArray = require(`..${slash}lib${slash}text.js`).musings(points, bodyPart);
+    var container = document.getElementsByClassName("container")[0];
+    var table = document.createElement("table");
+    table.id = "table";
+    table.className = "musing_table";
+    musingArray.forEach(musing => {
+        var row = document.createElement("th")
+        var musingText = document.createElement("button");
+        musingText.innerHTML = musing;
+        musingText.addEventListener("click", musingButtonCallback);
+        musingText.className = "musing_button";
+        musingText.bodyPart = bodyPart;
+        row.appendChild(musingText);
+        table.appendChild(row);
+    })
+    table.style = "width: 50%";
+    container.appendChild(table);
+}
+
+function increasePoints(bodyPart, musing) {
+    points = require('./lib/text.js').increasePoints(points, bodyPart, musing);
+    console.log(points);
+}
+
+function createBodyButtons() {
+    console.log(available);
+    if (available.head) {
+        var headButton = document.createElement("button");
+        headButton.className = "headButton";
+        headButton.onclick = () => {
+            loadMusings("head");
+        };
+        var container = document.getElementsByClassName("container")[0];
+        container.appendChild(headButton);
+    }
+    if (available.torso) {
+        var torsoButton = document.createElement("button");
+        torsoButton.className = "torsoButton";
+        torsoButton.onclick = () => {
+            loadMusings("torso");
+        };
+        var container = document.getElementsByClassName("container")[0];
+        container.appendChild(torsoButton);
+    }
+    if (available.arms) {
+        var armsButton = document.createElement("button");
+        armsButton.className = "armsButton";
+        armsButton.onclick = () => {
+            loadMusings("arms");
+        };
+        var container = document.getElementsByClassName("container")[0];
+        container.appendChild(armsButton);
+    }
+    if (available.legs) {
+        var legsButton = document.createElement("button");
+        legsButton.className = "legsButton";
+        legsButton.onclick = () => {
+            loadMusings("legs");
+        };
+        var container = document.getElementsByClassName("container")[0];
+        container.appendChild(legsButton);
+        console.log(legsButton);
+    }
+    if (available.clipboard) {
+        var clipboardButton = document.createElement("button");
+        clipboardButton.className = "clipboardButton";
+        clipboardButton.onclick = () => {
+            loadMusings("head");
+        };
+        var container = document.getElementsByClassName("container")[0];
+        container.appendChild(clipboardButton);
+        console.log(clipboardButton);
+    }
+}
--- temp.js
+++ temp.js
@@ -1,12 +1,31 @@
-let text = "You: “The wound back here is an odd one, to say the least. Come take a look.”|Arthur leans in from the other side of the table, wincing slightly once he catches view of the matted tangle of blood and hair. He observes it for a moment, his eyes betraying some small fascination, then pulls himself gently away.|Arthur: “It looks like a wound right out of the textbook, frankly. I don’t think it could be any more clearly an object made a weapon in the spur of the moment; a crime born from passion turned sour; perhaps a lover’s quarrel turned deadly, with some femme fatale having had more than enough of our victim’s vagrant ways, turning in frustration to violen--”|You:“Arthur. If you’ll look a little more closely, you’ll see the ripped skin on the sides of the wound. These patterns extend further into the flesh, indicating some degree of serration and a relatively thin object.”|You: “Your theory is compelling, but impractical. Especially given the rest of the wounds on the body.”|Arthur gives a glance to the victim, seeming to wax poetic in his own world just a bit longer, trying to burn the image of his thorned rose into his memory. He writes something in his notes, his face downcast."
+let text = "You: “Okay. I really don’t need you getting all sanctimonious over this, especially when we’ve got a dead body in here.”|You: “And I especially don’t need you policing me about what is and isn’t goth, especially when you’re so incredibly wrong about it.”|Arthur: “Wait, what is this about--”|You: “You know damn well what this is about, you fool.”|You: “You absolute goddamn loon.”|You: “You soft brain soyboy.”|You: “Parading all up in here with a brain that looks like raw chicken breast all up in MY morgue and trying to tell me what goth it.”|You: ““|You: “Hey! Hey Arthur!”|“Arthur stares back at me nervously.”|You: “Hey Arthur! I’ve got a message for you!”|Arthur: “OKay..?”|You: “Hey Arthur! Fuck you!”|You: “And if you see your parents, I want you to tell them something for me.”|A pause.|You: “I want you to tell them:”|You: “Fuck you!”|You: “Look at the audacity on this bitch. Comin’ up all in my morgue and trying to tell me what IS and ISN’T goth.”|You: “Guess what, boy“?|Arthur is silent, his expression sunken behind his mask.|You: “How are you the least pleasant person in this room when the other one is a goddamn corpse, Arthur?”|Whew. I pause and catch my breath.|What was I doing again? I look down. In the heat of the moment, I had plunged my hand into the open wound on the victim’s head.|My hand is caught in an instinctive death grip around the victim’s brain. It squishes about under my fingers.|Still a better brain than Arthur’s. No contest."
 
 console.log(text.split("|"));
 
 [
-    "You: “The wound back here is an odd one, to say the least. Come take a look.”",
-    "Arthur leans in from the other side of the table, wincing slightly once he catches view of the matted tangle of blood and hair. He observes it for a moment, his eyes betraying some small fascination, then pulls himself gently away.",
-    "Arthur: “It looks like a wound right out of the textbook, frankly. I don’t think it could be any more clearly an object made a weapon in the spur of the moment; a crime born from passion turned sour; perhaps a lover’s quarrel turned deadly, with some femme fatale having had more than enough of our victim’s vagrant ways, turning in frustration to violen--”",
-    "You:“Arthur. If you’ll look a little more closely, you’ll see the ripped skin on the sides of the wound. These patterns extend further into the flesh, indicating some degree of serration and a relatively thin object.”",
-    "You: “Your theory is compelling, but impractical. Especially given the rest of the wounds on the body.”",
-    "Arthur gives a glance to the victim, seeming to wax poetic in his own world just a bit longer, trying to burn the image of his thorned rose into his memory. He writes something in his notes, his face downcast."
-]
+    "You: “Okay. I really don’t need you getting all sanctimonious over this, especially when we’ve got a dead body in here.”",
+    "You: “And I especially don’t need you policing me about what is and isn’t goth, especially when you’re so incredibly wrong about it.”",
+    "Arthur: “Wait, what is this about--”",
+    "You: “You know damn well what this is about, you fool.”",
+    "You: “You absolute goddamn loon.”",
+    "You: “You soft brain soyboy.”",
+    "You: “Parading all up in here with a brain that looks like raw chicken breast all up in MY morgue and trying to tell me what goth it.”",
+    "You: ““",
+    "You: “Hey! Hey Arthur!”",
+    "“Arthur stares back at me nervously.”",
+    "You: “Hey Arthur! I’ve got a message for you!”",
+    "Arthur: “OKay..?”",
+    "You: “Hey Arthur! Fuck you!”",
+    "You: “And if you see your parents, I want you to tell them something for me.”",
+    "A pause.",
+    "You: “I want you to tell them:”",
+    "You: “Fuck you!”",
+    "You: “Look at the audacity on this bitch. Comin’ up all in my morgue and trying to tell me what IS and ISN’T goth.”",
+    "You: “Guess what, boy“?",
+    "Arthur is silent, his expression sunken behind his mask.",
+    "You: “How are you the least pleasant person in this room when the other one is a goddamn corpse, Arthur?”",
+    "Whew. I pause and catch my breath.",
+    "What was I doing again? I look down. In the heat of the moment, I had plunged my hand into the open wound on the victim’s head.",
+    "My hand is caught in an instinctive death grip around the victim’s brain. It squishes about under my fingers.",
+    "Still a better brain than Arthur’s. No contest."
+  ]
